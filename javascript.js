0bff50754b0d1a72e5fdd69ddd06a9a46a2eeed7
Hiei <james.illius@yahoo.com>
2018-05-19 19:54:13 -0400

    real commit, started js and question display

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+$(document).ready(function(){
+
+$("#startGame").on("click", function (){
+$("#mainPageCard").replaceWith("<p class =titleHeader >What is Rick Grimes sons name</p>");
+
+})
+});
