8a162cf891e402481e81a3999b5613a1e093d252
Hiei <james.illius@yahoo.com>
2018-05-21 23:48:07 -0400

    fixed a couple small styling problmes, attempt at adding endgame function, not working

--- javascript.js
+++ javascript.js
@@ -52,39 +52,32 @@ var trivia = [{
     question: "Who is ricks wife?",
     a: " Michonne", b:"Andrea", c: "Maggie", d:"Lori",
     answer: "Lori",
-    images: "images/lori.gif"
-}, {
-    question: "Who was Rick Grimes's best friend before the apocalypse?",
-    a: " Michonne", b:"Negan", c: "Shane", d:"Maggie",
-    answer: "Shane"
-}, {
-    question: "Who was Rick Grimes's best friend before the apocalypse?",
-    a: " Michonne", b:"Negan", c: "Shane", d:"Maggie",
-    answer: "Shane"
-}, {
+    images: "images/rickandlori.gif"
+
+
 }
 ];
 
 
 
 //time clock start
-var countDown = 11;
-var userCorrectAnswers = 0;  //counter for correct answers
-var userWrongAnswers = 0;    //counter for wrong answers
-var correctAnswer = "";     //used to check users answer
-var questionNumber = 0;     //used to cycle the next question 
-var stopVar = "";
-var userAnswer = "";
-var timerIsOff = true;
-$(document).ready(function(){
-    $("#questionPage").hide();         // hides question screen before start is clicked
-
-$("#startGame").on("click", function (){       // start game button
-    countDownTimer();
-    $("#questionPage").show();         // shows first question 
-    $("#mainPageCard").hide();         // hides intro screen after start is clicked 
-    // my pride and joy function. surprisingly i did this without googles help at all. displays question and cycles throuhg using question number++
-    displayQuestionsAnswers(trivia[questionNumber].question,trivia[questionNumber].a,trivia[questionNumber].b,trivia[questionNumber].c,trivia[questionNumber].d, trivia[questionNumber].answer);
+    var countDown = 11;
+    var userCorrectAnswers = 0;  //counter for correct answers
+    var userWrongAnswers = 0;    //counter for wrong answers
+    var correctAnswer = "";     //used to check users answer
+    var questionNumber = 0;     //used to cycle the next question 
+    var stopVar = "";
+    var userAnswer = "";
+    var gameOver = false;
+    $(document).ready(function(){
+        $("#questionPage").hide();         // hides question screen before start is clicked
+        $("#statPage").hide();
+    $("#startGame").on("click", function (){       // start game button
+         countDownTimer();
+         $("#questionPage").show();         // shows first question 
+         $("#mainPageCard").hide();         // hides intro screen after start is clicked 
+          // my pride and joy function. surprisingly i did this without googles help at all. displays question and cycles throuhg using question number++
+          displayQuestionsAnswers(trivia[questionNumber].question,trivia[questionNumber].a,trivia[questionNumber].b,trivia[questionNumber].c,trivia[questionNumber].d, trivia[questionNumber].answer);
     
   
     
@@ -92,7 +85,8 @@ $("#startGame").on("click", function (){       // start game button
     $(".answerButtons").on("click",function (){
         
    // verifies user answer to to objects answer/question answer
-       userAnswer = $(this).text();
+        userAnswer = $(this).text();
+      
       if(userAnswer === correctAnswer){
           userCorrectAnswers++;
           window.clearInterval(stopVar);
@@ -103,52 +97,62 @@ $("#startGame").on("click", function (){       // start game button
                           
           setTimeout(function(){
             $("#questionPage").show();
-            $("#displayCorrectAnswer").hide();          
+            $("#displayCorrectAnswer").hide();    
+            if (questionNumber ===2)  {
+                endGame();
+            }   
+            else {
             displayQuestionsAnswers(trivia[questionNumber].question,trivia[questionNumber].a,trivia[questionNumber].b,trivia[questionNumber].c,trivia[questionNumber].d, trivia[questionNumber].answer);
             countDownTimer();
-            }, 5000);
+            }
+            }, 1000);
             
            
           updateStats();
           
+          
       }
-
+      
       else {
         window.clearInterval(stopVar);
-        $("#questionPage").hide(); //hides question page     
-        $("#displayCorrectAnswer").show();      //shows hidden div that holds the correct answer pictures or gifs         
-        showCorrectAnswer (); //function to insert correct answer and images/gifs
-        questionNumber ++;    // needed to cycle the questions and images/gifs  
+            $("#questionPage").hide(); //hides question page     
+            $("#displayCorrectAnswer").show();      //shows hidden div that holds the correct answer pictures or gifs         
+            showCorrectAnswer (); //function to insert correct answer and images/gifs
+            questionNumber ++;    // needed to cycle the questions and images/gifs  
                         
-        setTimeout(function(){
-          $("#questionPage").show();
-          $("#displayCorrectAnswer").hide();          
-          displayQuestionsAnswers(trivia[questionNumber].question,trivia[questionNumber].a,trivia[questionNumber].b,trivia[questionNumber].c,trivia[questionNumber].d, trivia[questionNumber].answer);
-          countDownTimer();
-          }, 5000);
+            setTimeout(function(){
+                $("#questionPage").show();
+                $("#displayCorrectAnswer").hide();    
+                if (questionNumber === 2)  {
+                    endGame();
+                }   
+                else {
+                displayQuestionsAnswers(trivia[questionNumber].question,trivia[questionNumber].a,trivia[questionNumber].b,trivia[questionNumber].c,trivia[questionNumber].d, trivia[questionNumber].answer);
+                countDownTimer();
+                }
+                }, 1000);
           
          
        
-          userWrongAnswers--;
+          userWrongAnswers++;
           updateStats();
-
-      }
-    
-
+         
+           }
+      
     })
 })
 });
 // 30 second count down timer for each question
 
- function countDownTimer(){
- stopVar = setInterval(function(){
+    function countDownTimer(){
+        stopVar = setInterval(function(){
      
-    if (timerIsOff && 0<countDown){
+    if (1<countDown){
          countDown--;
 
       $("#countDownTimerId").html("Time Remaining " + countDown);
     }    
-      else if (countDown === 0) {
+    else if (countDown === 1) {
         window.clearInterval(stopVar);
         $("#questionPage").hide(); //hides question page     
         $("#displayCorrectAnswer").show();      //shows hidden div that holds the correct answer pictures or gifs         
@@ -156,17 +160,19 @@ $("#startGame").on("click", function (){       // start game button
         questionNumber ++;    // needed to cycle the questions and images/gifs  
                         
         setTimeout(function(){
-          $("#questionPage").show();
-          $("#displayCorrectAnswer").hide();          
-          displayQuestionsAnswers(trivia[questionNumber].question,trivia[questionNumber].a,trivia[questionNumber].b,trivia[questionNumber].c,trivia[questionNumber].d, trivia[questionNumber].answer);
-          countDownTimer();
-          }, 5000);
-          
-         
-       
-        userWrongAnswers--;
-        
+            $("#questionPage").show();
+            $("#displayCorrectAnswer").hide();    
+            if (questionNumber === 2)  {
+                endGame();
+            }   
+            else {
+            displayQuestionsAnswers(trivia[questionNumber].question,trivia[questionNumber].a,trivia[questionNumber].b,trivia[questionNumber].c,trivia[questionNumber].d, trivia[questionNumber].answer);
+            countDownTimer();
+            }
+            }, 1000);
+        userWrongAnswers++;     
         updateStats();
+        endGame();
     }
 
      
@@ -178,14 +184,14 @@ $("#startGame").on("click", function (){       // start game button
 
  // drys up the code displays the questions, answers and holds correct answer
 function displayQuestionsAnswers(question, answer1, answer2, answer3, answer4, correct) {
-    
-$("#questionId").html(question);
-$("#answer1Id").html(answer1);
-$("#answer2Id").html(answer2);
-$("#answer3Id").html(answer3);
-$("#answer4Id").html(answer4);
-correctAnswer = correct;
-
+   
+        $("#questionId").html(question);
+        $("#answer1Id").html(answer1);
+        $("#answer2Id").html(answer2);
+        $("#answer3Id").html(answer3);
+        $("#answer4Id").html(answer4);
+        correctAnswer = correct;
+       
 
 
 
@@ -195,20 +201,20 @@ correctAnswer = correct;
 
 function showCorrectAnswer (){
     if(userAnswer === correctAnswer){
-    $("#correctAnswerText").html("You chose the correct answer " + correctAnswer);
+    $("#correctAnswerText").html("You chose the correct answer: " + correctAnswer);
     }
-    else if (countDown === 0){
-    $("#correctAnswerText").html("Times up. The correct answer is " + correctAnswer);
+    else if (countDown === 1){
+    $("#correctAnswerText").html("Times up. The correct answer is: " + correctAnswer);
     }
     else {
-        $("#correctAnswerText").html("You chose an incorrect answer. The correct answer is " + correctAnswer);
+        $("#correctAnswerText").html("You chose an incorrect answer. The correct answer is: " + correctAnswer);
 
     }
         
-   
+   // my code from the hangman game, takes image from the object and puts it into the correct answer div
     var img = document.createElement("img");
-    img.style.width = "525px";
-    img.style.height = "375px";
+    img.style.width = "600px";
+    img.style.height = "450px";
     img.setAttribute("src", trivia[questionNumber].image);
  
      var div = document.getElementById("correctAnswerImg");
@@ -225,5 +231,16 @@ function showCorrectAnswer (){
    function updateStats(){
        $(".stats").html("Correct questions answered: " + userCorrectAnswers + "\n Incorrect questions answered: " + userWrongAnswers );
        countDown = 11;
+   
+
+      
        
-   }
+   };
+
+function endGame(){
+  
+    $("#questionPage").hide();
+    $("#statPage").show();
+    $("#statPageHeader").html("Correct questions answered: " + userCorrectAnswers + "\n Incorrect questions answered: " + userWrongAnswers + "\n Total questions: " + questionNumber-1);
+    };
+ 
